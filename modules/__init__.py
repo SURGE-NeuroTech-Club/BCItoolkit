fcb3b7665891e2ff47479a0dfd4a81737bcb1eca
MaxMascini <94568949+MaxMascini@users.noreply.github.com>
2024-10-21 16:03:00 -0300

    start working on visualization module + created jupyter notebook for testing with streamed recorded data

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,6 +1,7 @@
 from modules.brainflow_stream import *
 from modules.filtering import *
 from modules.brainflow_filtering import *
-from modules.segmentation import *
+# from modules.segmentation import *
 from modules.classification import *
 from modules.ssvep_stim import *
+from modules.visualization import *
